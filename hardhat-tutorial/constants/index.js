f8464d168b95ae5c975071d8682fd360a00127f8
Sneh Koul <snehkoul1999@gmail.com>
2022-01-04 17:41:56 -0500

    metadata url

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x98f5c62F7b5Bc735a64942dBec9456954c7a368A";
+// URL for the Crypto Devs NFT Metadata
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
