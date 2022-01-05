d2e80cd58c6e7c14e38c81c8c1bc99b00e80a4d8
Sneh Koul <snehkoul1999@gmail.com>
2022-01-04 21:26:59 -0500

    update the meta

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -349,7 +349,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Crypto Devs</title>
-        <meta name="description" content="Whitelist-Dapp" />
+        <meta name="description" content="NFT-Collection-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.main}>
