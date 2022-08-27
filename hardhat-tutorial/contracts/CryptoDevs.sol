aa8fb6f2d602ef4cf8b1b19cdb0d025460e592d0
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-08-27 06:05:16 +0200

    removed virtual from function header

--- hardhat-tutorial/contracts/CryptoDevs.sol
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -89,7 +89,7 @@ contract CryptoDevs is ERC721Enumerable, Ownable {
     * @dev _baseURI overides the Openzeppelin's ERC721 implementation which by default
     * returned an empty string for the baseURI
     */
-    function _baseURI() internal view virtual override returns (string memory) {
+    function _baseURI() internal view override returns (string memory) {
         return _baseTokenURI;
     }
      
